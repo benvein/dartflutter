96e5e5dadced6757094cfa4090cb7b28e5ee9307
benvein <kokobence2006@gmail.com>
2025-10-15 10:02:08 +0200

    doga elkezdve

---
+++ dolgozat.dart
@@ -0,0 +1,19 @@
+void main() {}
+
+String headTail(String text) {
+  String kesz = "";
+
+  return kesz;
+}
+
+String evenFizz(int number) {
+  return "";
+}
+
+List<int> likeFibo(int number) {
+  return [];
+}
+
+String vowelLower(String txt) {
+  return "";
+}
